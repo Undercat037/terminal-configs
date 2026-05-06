1693b8232394f61d3e0917fcc5dcd92b8a44e258
Deltacat-terminal <undercat037@gmail.com>
2026-05-06 22:16:17 +0300

    fastCommit

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -113,8 +113,6 @@ if status is-interactive
     alias .....="cd ../../../.."
     alias ......="cd ../../../../.."
 
-    alias cd='z'
-
     alias dl="cd ~/Downloads"
     alias doc="cd ~/Documents"
     alias dt="cd ~/Desktop"
